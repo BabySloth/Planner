91fdccba4cd5ae342e8ebb6bc1da72402ad0d409
BabySloth <williamcao999@gmail.com>
2018-08-11 13:57:31 -0400

    More features to dashboard added

--- src/main/java/view/changing/DashBoard.java
+++ src/main/java/view/changing/DashBoard.java
@@ -2,14 +2,21 @@ package view.changing;
 
 import helper.Fonts;
 import helper.Measurement;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
 import view.BasicView;
 import view.VIEWS;
+
+import java.time.LocalDate;
 
 public class DashBoard extends VBox implements BasicView {
     private final StackPane parent;
@@ -36,9 +43,19 @@ public class DashBoard extends VBox implements BasicView {
 
         // Previous goes on top
         //if(previousView != null)
-            getChildren().add(generatePreviousContainer());
+        getChildren().add(generatePreviousContainer()); //TODO: fix and indent
 
-        getChildren().addAll();
+        // List of all options
+        HBox regularContent = new HBox(calendarChange());
+        StackPane contents = new StackPane(regularContent, contentBackground());
+        ScrollPane regularHolder = new ScrollPane(contents);
+
+        // Design
+        regularHolder.getStyleClass().add("ScrollPane");
+        regularHolder.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        regularHolder.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+
+        getChildren().add(regularHolder);
     }
 
     private StackPane generatePreviousContainer(){
@@ -55,8 +72,53 @@ public class DashBoard extends VBox implements BasicView {
         return holder;
     }
 
-    private void generateContainer(){
+    private HBox calendarChange(){
+        VIEWS view = VIEWS.CALENDAR;
+        LocalDate now = LocalDate.now();
+        HBox container = generateContainer();
 
+        // Content
+        Label name = horizontalRegularLabel(view.toString(), 40);
+        String dateText = String.format("%s %d", now.getMonth().toString().substring(0, 3), now.getDayOfMonth());
+        Label date = horizontalRegularLabel(dateText, 40);
+
+        container.getChildren().addAll(name, date);
+        return container;
+    }
+
+    private HBox generateContainer(){
+        HBox holder = new HBox();
+
+        // Design
+        holder.setPrefSize(Measurement.DashBoard.REGULAR_WIDTH, Measurement.DashBoard.REGULAR_HEIGHT);
+        holder.setStyle("-fx-background-color: yellow;");
+        holder.setPadding(new Insets(0, 10, 0, 10));
+
+        return holder;
+    }
+
+    private Label horizontalRegularLabel(String text, double width){
+        Label label = new Label();
+
+        // Text
+        StringBuilder displayText = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            displayText.append(text.charAt(i)).append("\n");
+        }
+        label.setText(displayText.toString());
+
+        // Design
+        label.setStyle(Fonts.Style.getRoboto(30));
+        label.setPrefSize(width, Measurement.DashBoard.REGULAR_HEIGHT);
+        label.setAlignment(Pos.TOP_LEFT);
+
+        return label;
+    }
+
+    private Pane contentBackground(){
+        Pane background = new Pane();
+
+        return background;
     }
 
     public void setPreviousView(VIEWS previousView) {
